19fe0f5c6fcd9ca37945b3f9be57dee56e3d983d
jaroslavtyc <mail@jaroslavtyc.com>
2015-11-29 19:14:28 +0100

    Boolean enum interface can be used as enum interface

--- Doctrineum/Boolean/BooleanEnumInterface.php
+++ Doctrineum/Boolean/BooleanEnumInterface.php
@@ -1,9 +1,10 @@
 <?php
 namespace Doctrineum\Boolean;
 
+use Doctrineum\Scalar\EnumInterface;
 use Granam\Boolean\BooleanInterface;
 
-interface BooleanEnumInterface extends BooleanInterface
+interface BooleanEnumInterface extends BooleanInterface, EnumInterface
 {
 
 }
